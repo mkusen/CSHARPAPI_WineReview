efc7027bce59820a7f03636cc79e8e75ce1366be
mkusen <kusen.mario@gmail.com>
2024-09-26 14:00:13 +0200

    rad na cikličnoj

--- Controllers/CiklicnaController.cs
+++ Controllers/CiklicnaController.cs
@@ -1,5 +1,5 @@
 ﻿
-using CSHARPAPI_WineReview.Models;
+using CSHARPAPI_WineReview.Misc;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -8,95 +8,257 @@ namespace CSHARPAPI_WineReview.Controllers
     public class CiklicnaController : ControllerBase
     {
 
-        public class NumberWithDirection
-        {
-            public int Number { get; set; }
-            public List<bool> Directions { get; set; } // true - horizontal, false - vertical
-        }
-
-        public class Ciklicna
-        {
-            public NumberWithDirection[,] Matrix { get; set; }
-        }
 
         [HttpGet]
         [Route("api/v1/[controller]")]
         public IActionResult GetSpiralMatrix(int rows, int cols)
         {
+
             // Inicijalizacija matrice
-            NumberWithDirection[,] matrix = new NumberWithDirection[rows, cols];
+            NumberWithDirectionModel[,] matrix = new NumberWithDirectionModel[rows, cols];
+
             int num = 1;
             int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
 
             // Popunjavanje matrice u spiralnom poretku
             while (top <= bottom && left <= right)
             {
+
                 // Popunjavanje donjeg reda zdesna nalijevo
-                for (int i = right; i >= left; i--)
+                for (int a = right; a >= left; a--)
                 {
-                    matrix[bottom, i] = new NumberWithDirection
+                    //prvi broj
+                    if (a == right)
                     {
-                        Number = num++,
-                        Directions = new List<bool> { true, false } // horizontal, vertical
-                    };
+                        matrix[bottom, a] = new NumberWithDirectionModel
+                        {
+                            Number = num++,
+                            Left = true,
+                            Right = false,
+                            Up = false,
+                            Down = false,
+                            Color = "#FFFFFFFF",
+                            Position = "RTL"
+                        };
+
+                    }
+                    //zadnji broj
+                    else if (a == left)
+                    {
+                        matrix[bottom, a] = new NumberWithDirectionModel
+                        {
+                            Number = num++,
+                            Left = false,
+                            Right = true,
+                            Up = true,
+                            Down = false,
+                            Color = "#FF000000",
+                            Position = "RTL"
+                        };
+                    }
+
+                    //svi između
+                    else
+                    {
+                        matrix[bottom, a] = new NumberWithDirectionModel
+                        {
+                            Number = num++,
+                            Left = true,
+                            Right = true,
+                            Up = false,
+                            Down = false,
+                            Color = "#FF000000",
+                            Position = "RTL"
+                        };
+
+                    }
+
                 }
                 bottom--;
 
+
                 // Popunjavanje lijeve kolone odozdo prema gore
-                for (int i = bottom; i >= top; i--)
+                for (int b = bottom; b >= top; b--)
                 {
-                    matrix[i, left] = new NumberWithDirection
+                    //prvi broj
+                    if (b == bottom)
                     {
-                        Number = num++,
-                        Directions = new List<bool> { false, true } // vertical, horizontal
-                    };
+
+                        matrix[b, left] = new NumberWithDirectionModel
+                        {
+                            Number = num++,
+                            Left = false,
+                            Right = false,
+                            Up = true,
+                            Down = true,
+                            Color = "#FF000000",
+                            Position = "up"
+                        };
+                    }
+
+                    //zadnji broj
+                    else if (b == top)
+                    {
+                        matrix[b, left] = new NumberWithDirectionModel
+                        {
+                            Number = num++,
+                            Left = false,
+                            Right = true,
+                            Up = false,
+                            Down = true,
+                            Color = "#FF000000",
+                            Position = "up"
+                        };
+                    }
+                    //svi između
+                    else
+                    {
+                        matrix[b, left] = new NumberWithDirectionModel
+                        {
+                            Number = num++,
+                            Left = true,
+                            Right = true,
+                            Up = false,
+                            Down = false,
+                            Color = "#FF000000",
+                            Position = "up"
+                        };
+                    }
+                   
                 }
                 left++;
 
                 // Popunjavanje gornjeg reda slijeva nadesno
                 if (top <= bottom)
                 {
-                    for (int i = left; i <= right; i++)
+                    for (int c = left; c <= right; c++)
                     {
-                        matrix[top, i] = new NumberWithDirection
+                        //prvi broj
+                        if (c == left)
                         {
-                            Number = num++,
-                            Directions = new List<bool> { true, false } // horizontal, vertical
-                        };
+                            matrix[top, c] = new NumberWithDirectionModel
+                            {
+
+                                Number = num++,
+                                Left = true,
+                                Right = true,
+                                Up = false,
+                                Down = false,
+                                Color = "#FF000000",
+                                Position = "LTR"
+                            };
+                        }
+                        //zadnji broj
+                        else if (c == right)
+                        {
+                            matrix[top, c] = new NumberWithDirectionModel
+                            {
+
+                                Number = num++,
+                                Left = true,
+                                Right = false,
+                                Up = false,
+                                Down = true,
+                                Color = "#FF000000",
+                                Position = "LTR"
+                            };
+                        }
+
+                        //svi između
+                        else
+                        {
+                            matrix[top, c] = new NumberWithDirectionModel
+                            {
+
+                                Number = num++,
+                                Left = true,
+                                Right = true,
+                                Up = false,
+                                Down = false,
+                                Color = "#FF000000",
+                                Position = "LTR"
+                            };
+
+                        }
+                       
                     }
                     top++;
-                }
+
+                }              
 
                 // Popunjavanje desne kolone odozgo prema dolje
                 if (left <= right)
                 {
-                    for (int i = top; i <= bottom; i++)
+                    for (int d = top; d <= bottom; d++)
                     {
-                        matrix[i, right] = new NumberWithDirection
+
+                        //prvi broj
+                        if (d == top)
                         {
-                            Number = num++,
-                            Directions = new List<bool> { false, true } // vertical, horizontal
-                        };
+                            matrix[d, top] = new NumberWithDirectionModel
+                            {
+
+                                Number = num++,
+                                Left = false,
+                                Right = false,
+                                Up = true,
+                                Down = true,
+                                Color = "#FF000000",
+                                Position = "down"
+
+                            };
+                        }
+                        //zadnji broj
+                        else if (d == bottom)
+                        {
+                            matrix[d, top] = new NumberWithDirectionModel
+                            {
+
+                                Number = num++,
+                                Left = true,
+                                Right = false,
+                                Up = true,
+                                Down = false,
+                                Color = "#FFFF0000",
+                                Position = "down"
+
+                            };
+                        }
+
+                        //svi između
+                        else
+                        {
+                            matrix[d, top] = new NumberWithDirectionModel
+                            {
+
+                                Number = num++,
+                                Left = false,
+                                Right = false,
+                                Up = true,
+                                Down = true,
+                                Color = "#FF000000",
+                                Position = "down"
+
+                            };
+                        }                      
                     }
                     right--;
                 }
+                             
+
             }
 
-            // Kreiraj objekt Ciklicna i postavi matricu
-            var matrixNumbers = new Ciklicna
-            {
-                Matrix = matrix
-            };
-
             // Serijalizacija sa Newtonsoft.Json
-            string jsonResult = JsonConvert.SerializeObject(matrixNumbers);
+            string jsonResult = JsonConvert.SerializeObject(matrix);
 
             // Vraćanje rezultata kao JSON string
             return Content(jsonResult, "application/json");
+
         }
-
     }
+}
 
-}   
+
+
 
 
---
+++ Misc/NumberWithDirectionModel.cs
@@ -0,0 +1,18 @@
+﻿namespace CSHARPAPI_WineReview.Misc
+{
+    public class NumberWithDirectionModel
+    {
+        public int Number { get; set; }
+
+        // true - line, false - without line
+        public bool Left { get; set; }
+        public bool Right { get; set; }
+        public bool Up { get; set; }
+        public bool Down { get; set; }
+
+        //color for number field 
+        public string? Color { get; set; }
+
+        public string? Position { get; set; }
+    }
+}
