ffe48a03eda135828e4e7bf165756534d4c0a27a
mkusen <kusen.mario@gmail.com>
2024-10-05 09:07:52 +0200

    riješen DTO - vježbe 5-10-24

--- Mapping/WineReviewMappingProfile.cs
+++ Mapping/WineReviewMappingProfile.cs
@@ -11,14 +11,13 @@ namespace CSHARPAPI_WineReview.Mapping
             //mapping creation for TastingController: source, destination
             //potrebno dodati još ostale podatke 
             CreateMap<Tasting, TastingDTORead>()
-                 .ForMember(dest => dest.Wine,
-                opt => opt.MapFrom(src => src.Wine.WineName + " " + src.Wine.Maker + " " + src.Wine.YearOfHarvest + " " + src.Wine.Price)).
-                ForMember(dest => dest.Reviewer,
-                opt => opt.MapFrom(src => src.Reviewer.FirstName + " " + src.Reviewer.LastName)).
-                ForMember(dest => dest.EventPlace,
-                opt => opt.MapFrom(src => src.EventPlace.Country + " " + src.EventPlace.City + " " + src.EventPlace.PlaceName + " " + src.EventPlace.EventName)).
-                ForMember(dest => dest.Id,
-                opt => opt.MapFrom(src => src.Review + " " + src.EventDate));
+                 .ForCtorParam("WineName",
+                opt => opt.MapFrom(src => src.Wine.WineName + " " + src.Wine.Maker + " " + src.Wine.YearOfHarvest + " " + src.Wine.Price))
+                .ForCtorParam("ReviewerName",
+                opt => opt.MapFrom(src => src.Reviewer.FirstName + " " + src.Reviewer.LastName))
+                .ForCtorParam("EventName",
+                opt => opt.MapFrom(src => src.EventPlace.Country + " " + src.EventPlace.City + " " + src.EventPlace.PlaceName + " " + src.EventPlace.EventName));
+               
 
 
 
--- Models/DTO/TastingDTORead.cs
+++ Models/DTO/TastingDTORead.cs
@@ -7,20 +7,12 @@
             string Review,
             DateTime EventDate,
             //user (reviewer) by FK 'id_reviewer' taken from 'reviewer' table
-            string Reviewer,
-            string FirstName,
-            string LastName,
+            string ReviewerName,
             //wine by FK 'id_wine' taken from 'wine' table
-            string Wine,
-            string Maker,
+          
             string WineName,
-            string YearOfHarvest,
-            decimal? Price,
             //event_places by FK 'id_event_place' taken from 'event_places' table
-            string EventPlace,
-            string Country,
-            string City,
-            string PlaceName,
+    
             string? EventName
 
         );
